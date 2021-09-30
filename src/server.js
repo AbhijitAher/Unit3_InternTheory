5794f320acdbf6023a51bf761d16c6ac344e6b3f
AbhijitAher <seabhi.aher@gmail.com>
2021-09-30 16:22:09 +0530

    server file updated, controllers removed, view folder and files created

--- src/server.js
+++ src/server.js
@@ -2,35 +2,90 @@ const express = require('express');
 
 const app = express();
 
-app.use(express.json());
+const router = express.Router();
 
+app.use(express.json());
 
 const connect = require("./configs/db");
 
+app.set("view engine", "ejs");
 
-const internships_controller = require("./controllers/internships.controller")
+const Type = require("./models/type.model");
+const City = require("./models/city.model");
+const Category = require("./models/category.model");
+const Internship = require("./models/internships.model");
 
-const city_controller = require("./controllers/citys.controller")
 
-const type_controller = require("./controllers/types.controller")
+// const internships_controller = require("./controllers/internships.controller")
+// const course_controller = require("./controllers/courses.controller")
 
-const category_controller = require("./controllers/categorys.controller")
+// GET Requests
 
-const course_controller = require("./controllers/courses.controller")
+app.get("/InternTheory", async (req, res) => {
 
+    const types_data = await Type.find().lean().exec();
+    const cities_data = await City.find().lean().exec();
+    const category_data = await Category.find().lean().exec();
+
+    res.render("Home", {            // name of file in views
+        types_data: types_data, 
+        cities_data: cities_data,
+        category_data: category_data,                // data collected from router.get await function
+    })
+})
+
+app.get("/AllInternships", async (req, res) => {
+
+    const internships_data = await Internship.find().lean().exec();
+    console.log("AllInternships")
+    res.render("internship", {       // name of file in views
+        internships_data: internships_data        // data collected from router.get await function
+    })
+})
+
+
+// POST Requests
+
+router.post("", async (req, res) => {
+
+    const types = await Type.create(req.body);
+   // console.log(types);
+    return res.status(201).send(types);
+})
+
+router.post("", async (req, res) => {
+
+    const city = await City.create(req.body);
+   // console.log(city);
+    return res.status(201).send(city);
+})
+
+router.post("", async (req, res) => {
+
+    const category = await Category.create(req.body);
+   // console.log(category);
+    return res.status(201).send(category);
+})
+
+router.post("", async (req, res) => {
+
+    const internships = await Internship.create(req.body);
+   // console.log(internships);
+    return res.status(201).send(internships);
+})
 
 
 //console.log(internships_controller);
 
-app.use("/internships", internships_controller);
+// app.use("/internships", internships_controller);
 
-app.use("/citys", city_controller);
+// app.use("/types", type_controller);
 
-app.use("/categorys", category_controller);
+// app.use("/citys", city_controller);
 
-app.use("/types", type_controller);
+// app.use("/categorys", category_controller);
 
-app.use("/courses", course_controller);
+// app.use("/courses", course_controller);
 
 
 
@@ -38,4 +93,5 @@ app.use("/courses", course_controller);
 app.listen(2345, async ()=> {
     await connect();
     console.log("listening on port 2345");
+
 })
